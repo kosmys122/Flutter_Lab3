1e2e9e09ad18c842a839bf6de08d3a972de5470f
TishkaaPlay <tix.tot@yandex.ru>
2026-05-05 15:35:21 +0300

    Extract StyledText into a separate widget and file

---
+++ lib/styled_text.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+  Widget build(BuildContext context) {
+    return Text(
+      "Hello world!",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 32,
+      ),
+    );
+  }
+}
